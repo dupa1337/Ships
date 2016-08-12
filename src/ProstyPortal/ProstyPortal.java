f737ab2f91214ec48b4791ce9a365a12f408a52d
cerbin <bleble@d.com>
2016-08-13 01:09:46 +0200

    Implemented code for class ProstyPortal

--- src/ProstyPortal/ProstyPortal.java
+++ src/ProstyPortal/ProstyPortal.java
@@ -1,6 +1,23 @@
 package ProstyPortal;
 
 class ProstyPortal {
+    String sprawdz(String stringPole) {
+        int strzal = Integer.parseInt(stringPole);
 
+        String wynik = "pudlo";
 
+        for (int pole : polaPolozenia) {
+            if (strzal == pole) {
+                wynik = "trafiony";
+                iloscTrafien++;
+                break;
+            }
+        }
+        if (iloscTrafien == polaPolozenia.length) {
+            wynik = "zatopiony";
+        }
+        System.out.println(wynik);
+
+        return wynik;
+    }
 }
--- src/ProstyPortal/ProstyPortalTester.java
+++ src/ProstyPortal/ProstyPortalTester.java
@@ -3,20 +3,14 @@ package ProstyPortal;
 public class ProstyPortalTester {
     public static void main(String[] args) {
         ProstyPortal wit = new ProstyPortal();
-
         int[] polozenia = {2, 3, 4};
-
         wit.setPolaPolozenia(polozenia);
-
         String wybranePole = "2";
-
         String wynik = wit.sprawdz(wybranePole);
-
         String wynikTestu = "niepowodzenie";
         if (wynik.equals("trafiony")) {
             wynikTestu = "zakonczony pomyslnie";
         }
-
         System.out.println(wynikTestu);
     }
 }
