fd23526449b68cbc50dd5cf4ba46813d15b40b3c
cerbin <bleble@d.com>
2016-08-14 21:57:24 +0200

    Created final code.

--- src/ProstyPortal/ProstyPortalTester.java
+++ src/ProstyPortal/ProstyPortalTester.java
@@ -1,16 +1,31 @@
 package ProstyPortal;
 
+import java.util.Scanner;
+
 public class ProstyPortalTester {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int iloscRuchow = 0;
         ProstyPortal wit = new ProstyPortal();
-        int[] polozenia = {2, 3, 4};
+        int poczatekPortalu = (int) (Math.random() * 5);
+        int[] polozenia = {poczatekPortalu, poczatekPortalu + 1, poczatekPortalu + 2};
         wit.setPolaPolozenia(polozenia);
-        String wybranePole = "2";
-        String wynik = wit.sprawdz(wybranePole);
-        String wynikTestu = "niepowodzenie";
-        /*if (wynik.equals("trafiony")) {
-            wynikTestu = "zakonczony pomyslnie";
+        boolean czyIstnieje = true;
+        while (czyIstnieje) {
+            System.out.println("Podaj pole");
+            String wybranePole = scanner.nextLine();
+            iloscRuchow ++;
+            if (wit.sprawdz(wybranePole).equals("zatopiony")) {
+                czyIstnieje = false;
+                System.out.println("Zrobiles " + iloscRuchow + " ruchow");
+            }
         }
-        System.out.println(wynikTestu);*/
     }
 }
+      /*  String wybranePole = "2";
+        String wynik = wit.sprawdz(wybranePole);
+        if (wynik.equals("trafiony")) {
+            wynikTestu = "zakonczony pomyslnie";
+        }
+        System.out.println(wynikTestu);
+        String wynikTestu = "niepowodzenie";*/
