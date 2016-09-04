908a2b12da520ff57ea9eb2a8049f773aa44b4e9
cerbin <bleble@d.com>
2016-09-04 23:42:55 +0200

    Clean comment.

--- src/ProstyPortal/ProstyPortal.java
+++ src/ProstyPortal/ProstyPortal.java
@@ -26,32 +26,3 @@ class ProstyPortal {
         return wynik;
     }
 }
-
-/*
-
-class ProstyPortal {
-    int[] polaPolozenia;
-    int iloscTrafien;
-    boolean[] tablicaLogiczna;
-
-    public void setPolaPolozenia(int[] ppol) {
-        polaPolozenia = ppol;
-    }
-
-    String sprawdz(String stringPole) {
-        int strzal = Integer.parseInt(stringPole);
-        String wynik = "pudlo";
-        for (int pole : polaPolozenia) {
-            if (strzal == pole) {
-                wynik = "trafiony";
-                iloscTrafien++;
-                break;
-            }
-        }
-        if (iloscTrafien == polaPolozenia.length) {
-            wynik = "zatopiony";
-        }
-        System.out.println(wynik);
-        return wynik;
-    }
-}*/
